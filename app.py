ae0ded37fca25c8933d0a5bbfeb42b1f18a18444
Alessandro Fabiani <61310291+sandrofab@users.noreply.github.com>
2024-05-27 17:45:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ def hello_world():
     return 'Hello, World! dajeee'
 
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
