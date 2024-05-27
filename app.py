b7d7997b5019c5ad79332553ea07be59e1c9485d
Alessandro Fabiani <61310291+sandrofab@users.noreply.github.com>
2024-05-27 16:14:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, render_template
+import pandas as pdapp = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! dajeee'
+
+
